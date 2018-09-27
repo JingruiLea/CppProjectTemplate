77c97885dd94f56486be02cb1f8ee1edcfa44220
Jingrui Li <1079805974@qq.com>
2018-09-28 00:41:13 +0800

    first commit

--- build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/jingruili/OneDrive/ChessE
-# Build directory: /Users/jingruili/OneDrive/ChessE/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("test")
--- build/test/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/jingruili/OneDrive/ChessE/test
-# Build directory: /Users/jingruili/OneDrive/ChessE/build/test
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-add_test(utils_test "/Users/jingruili/OneDrive/ChessE/bin/test/utils_test")
